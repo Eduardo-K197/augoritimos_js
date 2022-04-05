dfaca46c641ddbe7f34e98302c0c162febdcc56a
Eduardo.K197 <44842173+Eduardo-K197@users.noreply.github.com>
2022-04-05 19:56:17 -0300

    up

---
+++ Fundamentos/Inicio/desafio_area.js
@@ -0,0 +1,7 @@
+let PI = 3.14
+const raio = 10
+let areaCirc = PI * raio * raio
+console.log("Ò valor da area é:",areaCirc,"m2")
+
+areaCirc = Math.PI * raio * raio
+console.log("Ò valor da area é:",areaCirc,"m2")
---
+++ Fundamentos/Inicio/desafio_troca.js
@@ -0,0 +1,14 @@
+let a = 7
+let b = 94
+
+console.log(a, b)
+
+/*a = 94
+b = 7
+
+console.log(a, b)*/
+
+let temp = a
+a = b
+b = temp
+console.log(a, b)
