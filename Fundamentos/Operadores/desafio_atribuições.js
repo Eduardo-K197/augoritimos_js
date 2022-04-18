0da6a6fc0ab85d3bbd56dcf2f090557ed65f2e57
Eduardo.K197 <44842173+Eduardo-K197@users.noreply.github.com>
2022-04-17 21:54:34 -0300

    up1.6

---
+++ Fundamentos/Operadores/desafio_atribuições.js
@@ -0,0 +1,7 @@
+let texto = 'Lista de aprovados\n'
+texto += '==================\n\n'
+texto += '1. Ana Silva\n'
+texto += '2. Pedro Alburquerque\n'
+texto += '3.Gilherme Pereira'
+
+console.log(texto)
