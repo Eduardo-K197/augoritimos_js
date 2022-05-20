c3cda018abbfafadfbd59d037d15a25664265100
Eduardo.K197 <44842173+Eduardo-K197@users.noreply.github.com>
2022-05-20 11:46:40 -0300

    up1.8

---
+++ Fundamentos/Operadores/index.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Busca Socio</title>
+</head>
+<body>
+<h2>Pesquisa de Socios</h2>
+<form action="busca.php" method="GET">
+    <label>Nome do socio</label>
+    <input type="text" name="nome_socio" size="50" placeholder="Ins>
+                <button style="width:100px;">Buscar</button>
+</form>
+</body>
+</html>
